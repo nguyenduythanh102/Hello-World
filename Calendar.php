1228d639009ca7e568aa388688456795f5b38781
tamanvn <tamanvnsp@gmail.com>
2017-02-15 20:10:28 +0700

    Add files via upload

---
+++ Calendar.php
@@ -0,0 +1,152 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'SINGLE_Calendar'              => 'Cần Làm'                       , 
+	'LBL_ADD_TASK'                 => 'Thêm Tác Vụ'                    , 
+	'LBL_ADD_EVENT'                => 'Thêm Sự Kiện'                   , 
+	'LBL_RECORDS_LIST'             => 'Xem Danh Sách'                   , 
+	'LBL_EVENTS'                   => 'Sự Kiện'                      , 
+	'LBL_TODOS'                    => 'Các Việc Phải Làm'                      , 
+	'LBL_CALENDAR_SETTINGS'        => 'Thiết Lập Lịch'           , // TODO: Review
+	'LBL_CALENDAR_SHARING'         => 'Chia sẽ lịch làm việc'            , // TODO: Review
+	'LBL_DEFAULT_EVENT_DURATION'   => 'Thời gian mặc định'      , // TODO: Review
+	'LBL_CALL'                     => 'Gọi'                        , // TODO: Review
+	'LBL_OTHER_EVENTS'             => 'Các Sự Kiện Khác'                , // TODO: Review
+	'LBL_MINUTES'                  => 'Phút'                     , // TODO: Review
+	'LBL_SELECT_USERS'             => 'Chọn người dùng'                , // TODO: Review
+	'LBL_EVENT_OR_TASK'            => 'Sự kiện / Tác Vụ'                ,
+	'LBL_TASK_INFORMATION'         => 'Thông Tin Tác Vụ'            , 
+    'LBL_EVENT_INFORMATION'        => 'Các Chi Tiết Sự Kiện'               ,
+	'Subject'                      => 'Chủ Đề'                     , 
+	'Start Date & Time'            => 'Ngày Bắt Đầu & Thời Gian'           , 
+	'Activity Type'                => 'Kiểu Hoạt Động'               , 
+	'Send Notification'            => 'Gửi Thông Báo'           , 
+	'Location'                     => 'Địa Điểm'                    , 
+	'End Date & Time'              => 'Ngày Kết Thúc & Thời Gian'             , 
+	'LBL_ACTIVITY_TYPES'           => 'Kiểu Hoạt Động'              , 
+	'LBL_CONTACTS_SUPPORT_END_DATE' => 'Ngày Kết Thúc Hỗ Trợ'            , 
+	'LBL_CONTACTS_BIRTH_DAY'       => 'Sinh Nhật Khách Hàng'               , 
+	'LBL_ADDED_CALENDARS'          => 'Thêm Lịch'             , // TODO: Review
+	'Call'                         => 'Gọi'                        , 
+	'Meeting'                      => 'Họp'                     , 
+	'Task'                         => 'Tác Vụ'                        , // TODO: Review
+	'Planned'                      => 'Kế Hoạch'                     , 
+        'Held'                         =>   'Held',
+        'Not Held'                     =>   'Not Held',
+	'Completed'                    => 'Hoàn Thành'                   , 
+	'Pending Input'                => 'Pending Input'               , 
+	'Not Started'                  => 'Không Bắt Đầu'                 , 
+	'Deferred'                     => 'Deferred'                    , 
+	'Medium'                       => 'Trung Bình'                      , 
+	'LBL_CHANGE_OWNER'             => 'Đổi Người Phụ Trách'                , 
+	'LBL_EVENT'                    => 'Sự Kiện'                       , 
+	'LBL_TASK'                     => 'Tác Vụ'                        , 
+	'LBL_TASKS'					   => 'Tác Vụ'						,
+	'LBL_CALENDAR_VIEW'            => 'Xem Lịch'               , 
+	'LBL_SHARED_CALENDAR'          => 'Chia Sẻ Lịch Làm Việc'             , // TODO: Review
+	'LBL_DAY0'                     => 'Chúa Nhật'                      , // TODO: Review
+	'LBL_DAY1'                     => 'Thứ Hai'                      , // TODO: Review
+	'LBL_DAY2'                     => 'Thứ Ba'                     , // TODO: Review
+	'LBL_DAY3'                     => 'Thứ Tư'                   , // TODO: Review
+	'LBL_DAY4'                     => 'Thứ Năm'                    , // TODO: Review
+	'LBL_DAY5'                     => 'Thứ Sáu'                      , // TODO: Review
+	'LBL_DAY6'                     => 'Thứ 7'                    , // TODO: Review
+	'first'                        => 'Đầu Tiên'                       , // TODO: Review
+	'last'                         => 'Cuối Cùng'                        , // TODO: Review
+	'LBL_DAY_OF_THE_MONTH'         => 'Ngày Của Tháng'            , // TODO: Review
+	'LBL_ON'                       => 'on'                          , // TODO: Review
+	'Daily'                        => 'Các Ngày'                      , // TODO: Review
+	'Weekly'                       => 'Các Tuần'                     , // TODO: Review
+	'Monthly'                      => 'Các Tháng'                    , // TODO: Review
+	'Yearly'                       => 'Năm'                        , // TODO: Review
+        'LBL_DEFAULT_STATUS_TYPE'      => 'Tình Trạng Mặc Định & Kiểu'       ,
+        'LBL_STATUS'                   => 'Tình Trạng'                      ,
+        'LBL_TYPE'                     => 'Kiểu',
+	//Fixing colors for Shared Calendar and My Calendar
+	'LBL_EDIT_COLOR' => 'Chỉnh Màu',
+	'LBL_ADD_CALENDAR_VIEW' => 'Thêm Lịch',
+	'LBL_SELECT_USER_CALENDAR' => 'Chọn Người Dùng Cho Lịch',
+	'LBL_SELECT_CALENDAR_COLOR' => 'Chọn Màu Của Lịch',
+	'LBL_EDITING_CALENDAR_VIEW' => 'Sửa Người Của Lịch',
+	'LBL_DELETE_CALENDAR' => 'Xóa Lịch',
+	'LBL_SELECT_ACTIVITY_TYPE' => 'Chọn Kiểu Hoạt Động',
+	'Tasks' => 'Tasks',
+);
+$jsLanguageStrings = array(
+	'LBL_ADD_EVENT_TASK'           => 'Thêm Sự Kiện / Tác Vụ'            , 
+	'JS_TASK_IS_SUCCESSFULLY_ADDED_TO_YOUR_CALENDAR' => 'Tác Vụ Đã Thành Công Trên Lịch Trình', // TODO: Review
+	'LBL_SYNC_BUTTON'              => 'Đồng Bộ Ngay'             , // TODO: Review
+	'LBL_SYNCRONIZING'             => 'Đang Đồng Bộ....'           , // TODO: Review
+	'LBL_NOT_SYNCRONIZED'          => 'Bạn Chưa Đồng Bộ', // TODO: Review
+	'LBL_FIELD_MAPPING'            => 'Field Mapping'               , // TODO: Review
+	'LBL_CANT_SELECT_CONTACT_FROM_LEADS' => 'Không Thể Tìm Được Đầu Mối Liên Quan', // TODO: Review
+        'JS_FUTURE_EVENT_CANNOT_BE_HELD' => 'Cannot Be Held For Future',
+	
+	//Calendar view label translation
+	'LBL_MONTH' => 'Tháng',
+	'LBL_TODAY' => 'Hôm Nay',
+	'LBL_DAY' => 'Ngày',
+	'LBL_WEEK' => 'Tuần',
+	
+	'LBL_SUNDAY' => 'Chúa Nhật',
+	'LBL_MONDAY' => 'Thứ Hai',
+	'LBL_TUESDAY' => 'Thứ Ba',
+	'LBL_WEDNESDAY' => 'Thứ Tư',
+	'LBL_THURSDAY' => 'Thứ Năm',
+	'LBL_FRIDAY' => 'Thứ Sáu',
+	'LBL_SATURDAY' => 'Thứ Bảy',
+	
+	'LBL_SUN' => 'CN',
+	'LBL_MON' => 'T.2',
+	'LBL_TUE' => 'T.3',
+	'LBL_WED' => 'T.4',
+	'LBL_THU' => 'T.5',
+	'LBL_FRI' => 'T.6',
+	'LBL_SAT' => 'T.7',
+	
+	'LBL_JANUARY' => 'Tháng Một',
+	'LBL_FEBRUARY' => 'Tháng Hai',
+	'LBL_MARCH' => 'Tháng Ba',
+	'LBL_APRIL' => 'Tháng Tư',
+	'LBL_MAY' => 'Tháng Năm',
+	'LBL_JUNE' => 'Tháng',
+	'LBL_JULY' => 'Tháng Bảy',
+	'LBL_AUGUST' => 'Tháng Tám',
+	'LBL_SEPTEMBER' => 'Tháng Chín',
+	'LBL_OCTOBER' => 'Tháng Mười',
+	'LBL_NOVEMBER' => 'Tháng Mưới Một',
+	'LBL_DECEMBER' => 'Tháng Mưới Hai',
+	
+	'LBL_JAN' => 'Th.1',
+	'LBL_FEB' => 'Th.2',
+	'LBL_MAR' => 'Th.3',
+	'LBL_APR' => 'Th.4',
+	'LBL_MAY' => 'Th.5',
+	'LBL_JUN' => 'Th.6',
+	'LBL_JUL' => 'Th.7',
+	'LBL_AUG' => 'Th.8',
+	'LBL_SEP' => 'Th.9',
+	'LBL_OCT' => 'Th.10',
+	'LBL_NOV' => 'Th.11',
+	'LBL_DEC' => 'Th.12',
+	
+	'LBL_ALL_DAY' => 'Tất Cả Ngày',
+	
+	'Mobile Call' => 'Gọi Di Động',
+	//End
+	
+	//Fixing colors for Shared Calendar and My Calendar
+	'JS_CALENDAR_VIEW_COLOR_UPDATED_SUCCESSFULLY' => 'Màu Của Người Dùng Cập Nhật Thành Công',
+	'JS_CALENDAR_VIEW_DELETE_CONFIRMATION' => 'Bạn có chắc muốn xóa Lịch của người dùng này không?',
+	'JS_CALENDAR_VIEW_ADDED_SUCCESSFULLY' => 'Lịch người dùng được thêm vào thành công',
+	'JS_CALENDAR_VIEW_DELETED_SUCCESSFULLY' => 'Lịch người dùng được xóa thành công',
+	'JS_NO_CALENDAR_VIEWS_TO_ADD' => 'Không có lịch người dùng nào được thêm',
+	'JS_EDIT_CALENDAR' => 'Sửa Lịch',
+);
---
+++ Contacts.php
@@ -0,0 +1,59 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$languageStrings = array(
+	'Contacts'                     => 'Khách Hàng'                    , 
+	'SINGLE_Contacts'              => 'Khách Hàng'                     , 
+	'LBL_ADD_RECORD'               => 'Thêm Khách Hàng'                 , 
+	'LBL_RECORDS_LIST'             => 'Danh Sách Khách Hàng'               , 
+	'LBL_CONTACT_INFORMATION'      => 'Thông Tin Khách Hàng'         , 
+	'LBL_CUSTOMER_PORTAL_INFORMATION' => 'Cổng Thông Tin Khách Hàng' , 
+	'LBL_IMAGE_INFORMATION'        => 'Hình Ảnh Thông Tin Khách Hàng:'  , 
+	'LBL_COPY_OTHER_ADDRESS'       => 'Sao Chép Địa Chỉ Giao Hàng'          , // TODO: Review
+	'LBL_COPY_MAILING_ADDRESS'     => 'Sao Chép Địa Chỉ Giao Hóa Đơn'        , // TODO: Review
+	'Office Phone'                 => 'Điện Thoại Văn Phòng'                , 
+	'Home Phone'                   => 'Điện Thoại Nhà Riêng'                  , 
+	'Title'                        => 'Chức Danh'                   , 
+	'Department'                   => 'Phòng-Ban'                  , 
+	'Birthdate'                    => 'Ngày Sinh'                   , 
+	'Reports To'                   => 'Báo Cáo Cho'                  , 
+	'Assistant'                    => 'Tên Người Trợ Lý-Assistant Name'              , 
+	'Assistant Phone'              => 'Điện Thoại Trợ Lý-Assistant Phone'             , 
+	'Do Not Call'                  => 'Cấm Gọi'                 , 
+	'Reference'                    => 'Reference'                   , 
+	'Portal User'                  => 'Portal User'                 , 
+	'Mailing Street'               => 'Tên Đường-Hóa Đơn Chứng Từ'              , 
+	'Mailing City'                 => 'Tỉnh Thành Phố-Hóa Đơn Chứng Từ'                , 
+	'Mailing State'                => 'Mailing State'              , 
+	'Mailing Zip'                  => 'Mã Vùng-Hóa Đơn Chứng Từ'            , 
+	'Mailing Country'              => 'Quốc Gia-Hóa Đơn Chứng Từ'             , 
+	'Mailing Po Box'               => 'Invoice P.O. Box'            , 
+	'Other Street'                 => 'Địa Chỉ Giao Hàng'             , 
+	'Other City'                   => 'Thành Phố'               , 
+	'Other State'                  => 'Quốc Gia'             , 
+	'Other Zip'                    => 'Zip Code'           , 
+	'Other Country'                => 'Quốc Gia Khác'            , 
+	'Other Po Box'                 => 'Delivery P.O. Box'              , 
+	'Contact Image'                => 'Hình Khách Hàng'               , 
+	'Mr.'                          => 'Mr.'                         , 
+	'Ms.'                          => 'Ms.'                         , 
+	'Mrs.'                         => 'Mrs.'                        , 
+	'Dr.'                          => 'Dr.'                         , 
+	'Prof.'                        => 'Prof.'                       , 
+	'User List'                    => 'User List'                   , 
+	'Contact Id' => 'Contact ID',
+    'Support Start Date'           => 'Ngày Bắt Đầu Hỗ Trợ'          ,
+    'Support End Date'             => 'Ngày Kết Thúc Hỗ Trợ'            ,
+);
+$jsLanguageStrings = array(
+	'LBL_SYNC_BUTTON'              => 'Đồng Bộ Bây Giờ'             , // TODO: Review
+	'LBL_SYNCRONIZING'             => 'Đang Đồng Bộ....'           , // TODO: Review
+	'LBL_NOT_SYNCRONIZED'          => 'Bạn Chưa Đồng Bộ', // TODO: Review
+	'LBL_FIELD_MAPPING'            => 'Field Mapping'               , // TODO: Review
+);
